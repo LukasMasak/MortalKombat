746b751dc7141933e5233ae89f0aa14bca161853
tutek1 <ondrej@kyzr.cz>
2024-03-17 14:35:24 +0100

    Menu start

--- Assets/Menu/AnimationEventController.cs
+++ Assets/Menu/AnimationEventController.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class AnimationEventController : MonoBehaviour
 {
+    [Tooltip("Used only if LoadGivenScene() is called.")]
+    [SerilizableField] private int sceneBuildIdxToLoad = -1;
+
     // Used in map and character choosing
     public void NextScene()
     {
@@ -15,5 +18,23 @@ public class AnimationEventController : MonoBehaviour
     public void PrevScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    // Load a scene with given index
+    public void LoadGivenScene()
+    {
+        if (sceneBuildIdxToLoad == -1) return;
+        SceneManager.LoadScene(sceneBuildIdxToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        //Application.Quit();
+    }
+
+    public void DebugPrint()
+    {
+        Debug.Log("Pressed button " + name);
     }
 }
