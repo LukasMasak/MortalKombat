8130782e2b32b63eb53ef38b169cb6c5f08322dc
tutek1 <ondrej@kyzr.cz>
2024-07-02 23:03:11 +0200

    Removed unnecessary prefab, added reference for player stats

--- Assets/EditCharacter/UIScripts/MenuManager.cs
+++ Assets/EditCharacter/UIScripts/MenuManager.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro _characterLabel;
+    [SerializeField] private TextMeshProUGUI _characterLabel;
+    [SerializeField] private Slider _hpSlider;
+    [SerializeField] private Slider _spdSlider;
+    [SerializeField] private Slider _dmglider;
+
+    [SerializeField] private Slider _atkSizeSlider;
+    [SerializeField] private Slider _atkXSlider;
+    [SerializeField] private Slider _atkYSlider;
+    [SerializeField] private Slider _atkFrameSlider;
 
 
     private List<CharacterData> _allCharacters = new List<CharacterData>();
