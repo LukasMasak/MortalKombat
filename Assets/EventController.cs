5b355775751c1d5fda03e07265a489459ab9adc9
tutek1 <ondrej@kyzr.cz>
2024-07-12 20:10:10 +0200

    FadeOut and FadeIn works by adding delay to EventCotroller

--- Assets/EventController.cs
+++ Assets/EventController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -13,41 +14,84 @@ public class EventController : MonoBehaviour
     // Used in map and character choosing
     public void NextScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // Scene over the max valid index
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+        {
+            Debug.LogWarning("Tried to load scene of index" + SceneManager.sceneCount + ", will not do anything!");
+            return;
+        }
+
+        StartCoroutine(DelayFunction(() => 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }));
     }
 
     // Used for DEBUG back button
     public void PrevScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        // Scene under the valid indicies
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Debug.LogWarning("Tried to load scene of index -1, will not do anything!");
+            return;
+        }
+
+        StartCoroutine(DelayFunction(() => 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }));
     }
 
     // Load a scene with given index
     public void LoadGivenScene(int sceneBuildIdxToLoad = -1)
     {
-        if (sceneBuildIdxToLoad == -1) return;
-        
-        SceneManager.LoadScene(sceneBuildIdxToLoad);
+        // Scene under the valid indicies
+        if (sceneBuildIdxToLoad < 0)
+        {
+            Debug.LogWarning("Tried to load scene of index -1, will not do anything!");
+            return;
+        }
+
+        // Scene over the max valid index
+        if (sceneBuildIdxToLoad >= SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.LogWarning("Tried to load scene of index" + SceneManager.sceneCount + ", will not do anything!");
+            return;
+        }
+
+        StartCoroutine(DelayFunction(() => 
+        {
+            SceneManager.LoadScene(sceneBuildIdxToLoad);
+        }));
     }
 
     // Quits the game
     public void QuitGame()
     {
         Debug.Log("Quitting game");
-        Application.Quit();
+
+        StartCoroutine(DelayFunction(() => 
+        {
+            Application.Quit();
+        }));
     }
 
     // DEBUG print to test functionality of event
     public void DebugPrint()
     {
-        Debug.Log("Pressed button " + name);
+        Debug.Log("Pressed button " + name + " before delay");
 
+        StartCoroutine(DelayFunction(() => 
+        {
+            Debug.Log("Pressed button " + name + " after delay");
+        }));
         
     }
 
-    /*private IEnumerator DelayCoroutine(UnityEvent eventToCall)
+    private IEnumerator DelayFunction(Action function)
     {
         yield return new WaitForSeconds(_operationDelay);
-        eventToCall.Invoke();
-    }*/
+        function();
+    }
 }
